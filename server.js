0a89ce9f381e80b6cc5256511238a48dfb2ec86e
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2023-12-13 21:26:38 +0530

    MongoDb updates

--- server.js
+++ server.js
@@ -1,71 +1,28 @@
 const express = require('express');
-const cors = require('cors');
-const sqlite3 = require('sqlite3').verbose();
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Enable CORS
-app.use(cors());
+const uri = 'mongodb+srv://prinuvinod:<password>@cluster0.qp044fw.mongodb.net/?retryWrites=true&w=majority'; // Replace with your MongoDB connection string
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Create a database and a table
-const db = new sqlite3.Database('inventory.db');
-db.run('CREATE TABLE IF NOT EXISTS inventory (itemId INTEGER PRIMARY KEY, itemname TEXT, quantity INTEGER)');
+let db;
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+    db = client.db('Inventory'); // Replace with your database name
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+}
 
-// Body parsing middleware to handle POST requests
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+connectToDatabase();
 
-// Add item to the database
-app.post('/addItem', (req, res) => {
-  const { itemname, quantity } = req.body;
+// Rest of your server code...
 
-  // Insert data into the database
-  db.run('INSERT INTO inventory (itemname, quantity) VALUES (?, ?)', [itemname, quantity], function (err) {
-    if (err) {
-      console.error('Error adding item:', err);
-      return res.status(500).json({ error: 'Error adding item' });
-    }
-
-    console.log('Item added successfully!');
-    res.json({ message: 'Item added successfully!', itemId: this.lastID });
-  });
-});
-
-// Delete item from the database
-app.delete('/deleteItem/:itemId', (req, res) => {
-  const { itemId } = req.params;
-
-  // Your delete logic here
-  // For example, using your SQLite3 database:
-  db.run('DELETE FROM inventory WHERE itemId = ?', [itemId], function (err) {
-    if (err) {
-      console.error('Error deleting item:', err);
-      return res.status(500).json({ error: 'Error deleting item' });
-    }
-
-    console.log('Item deleted successfully!');
-    res.json({ message: 'Item deleted successfully!' });
-  });
-});
-
-// Retrieve items from the database
-app.get('/getItems', (req, res) => {
-  // Select all data from the database
-  db.all('SELECT * FROM inventory', (err, rows) => {
-    if (err) {
-      console.error('Error retrieving items:', err);
-      return res.status(500).json({ error: 'Error retrieving items' });
-    }
-
-    res.json({ items: rows });
-  });
-});
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
